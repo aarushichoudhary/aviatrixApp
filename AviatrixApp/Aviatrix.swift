94a74c708d838c6a63084ded3d57a9f9c1831d1e
Apple <apple@Apples-MBP-4.attlocal.net>
2019-07-10 14:32:21 -0700

    complete iteration 1

--- AviatrixApp/Aviatrix.swift
+++ AviatrixApp/Aviatrix.swift
@@ -12,6 +12,10 @@ class Aviatrix {
     
     var author  =  ""
     
+    var data = AviatrixData()
+    
+    var currentLocation = "St. Louis"
+    //you need a value or will be nhppy no empty ""
     init(userName: String){
         author = userName
     }
@@ -28,15 +32,18 @@ class Aviatrix {
     }
     
     func flyTo(destination : String) {
-        
+        currentLocation = destination 
     }
-    
-    func distanceTo(target : String) {
-    
+    //arrow defines what we are looking for
+    func distanceTo(currentLocation : String, target : String) -> Int{
+        // exclamation is used to unwrap data, and tells the program that there is data on the other side for sure (you do no need to do this when computer recognizes it)
+        return data.knownDistances[currentLocation]![target]!
+     // ! saying comp doesnt know it's real. telling apple we want to wrap the data wthout questioning it.
+        // the data above goes into the sl dictionarykey, and goes into the phoenix and they key for phoneix
     }
     
     func knownDestinations() -> [String] {
-       return ["St. Louis"]
+       return ["St. Louis", "Phoenix", "Denver", "SLC"]
     }
 }
 
